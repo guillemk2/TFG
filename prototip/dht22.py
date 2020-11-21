39b3e3aa1f0638b8d7ff193f472a39d858a105e5
Guillem Peñafiel i Ràmia <pengu04@gmail.com>
2020-11-22 00:58:22 +0100

    sensor temp

---
+++ prototip/dht22.py
@@ -0,0 +1,30 @@
+import adafruit_dht
+import time
+import board
+
+# --------- User Settings ---------
+SENSOR_LOCATION_NAME = "Office"
+
+MINUTES_BETWEEN_READS = 10
+METRIC_UNITS = True
+# ---------------------------------
+
+dhtSensor = adafruit_dht.DHT22(15)
+
+while True:
+        try:
+                humidity = dhtSensor.humidity
+                temp_c = dhtSensor.temperature
+
+        except RuntimeError:
+                print("RuntimeError, trying again...")
+                continue
+                
+        
+        print(SENSOR_LOCATION_NAME + " Temperature(C)", temp_c)
+        
+        humidity = format(humidity,".2f")
+
+        print(SENSOR_LOCATION_NAME + " Humidity(%)", humidity)
+
+        time.sleep(MINUTES_BETWEEN_READS)
