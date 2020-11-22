d22a57c0cec828bba0c412465a03b16ae3bd274a
Guillem Peñafiel i Ràmia <pengu04@gmail.com>
2020-11-22 12:29:06 +0100

    arrays2 + globals

--- prototip/aux.py
+++ prototip/aux.py
@@ -11,7 +11,7 @@ import gpiozero
 # Constants
 from cfg import SYS_SIZE, POLL_FREQUENCY, POLL_TIME, IRRIGATION_TIME, DRY, WET, url, payload
 # Objectes
-from cfg import relays, buttons, sensors, sensors_vcc
+from cfg import relays, buttons, soil_sensors, soil_sensors_vcc
 # Variables globals
 from cfg import valves_t0, valves_t1
 
@@ -32,16 +32,18 @@ def init():
 	pause()
 
 def open_valve(n):
+	global valves_t0
 	if(relays[n].value == 0):
 		relays[n].on()
 		valves_t0[n] = time()
 		print("Obertura electrovàlvula", n)
 
 def close_valve(n):
+	global valves_t1
 	if(relays[n].value == 1):
 		relays[n].off()
 		valves_t1[n] = time()
-		print("Tancament electrovàlvula", n, "Temps de reg:", round(valves_t1[n]-valves_t0[n], 2), "segons\n")
+		print("Tancament electrovàlvula", n, "Temps de reg:", valves_t1[n]-valves_t0[n], "segons\n")
 
 def set_valves(status):
 	if status:
@@ -61,22 +63,22 @@ def button_released(btn):
 		if (buttons[i] == btn):
 			close_valve(i)
 
-def poll_sensors():
+def poll_soil_sensors():
 
 	while 1:
-		sensors_vcc[0].on()
-		sensors_vcc[1].on()
+		soil_sensors_vcc[0].on()
+		soil_sensors_vcc[1].on()
 		
-		if (sensors[0].value == DRY):
+		if (soil_sensors[0].value == DRY):
 			threading.Thread(target=irrigate, args=(buttons[0],)).start()
 			
-		if (sensors[1].value == DRY):
+		if (soil_sensors[1].value == DRY):
 			threading.Thread(target=irrigate, args=(buttons[1],)).start()
 		
 		sleep(POLL_TIME)
 				
-		sensors_vcc[0].off()
-		sensors_vcc[1].off()
+		soil_sensors_vcc[0].off()
+		soil_sensors_vcc[1].off()
 
 		post()
 		
--- prototip/cfg.py
+++ prototip/cfg.py
@@ -6,17 +6,17 @@ import gpiozero
 
 # CONSTANTS
 
-# Número de testos
+# Número de testos (mida del sistema)
 SYS_SIZE = 2
 
-# Duració dels events
-POLL_FREQUENCY = 5
-POLL_TIME = 0.25
-IRRIGATION_TIME = 1
+# Duració dels events (en segons)
+POLL_FREQUENCY = 5 		# Freqüència de lectura dels sensors
+POLL_TIME = 0.25		# Duració de la lectura dels sensors
+IRRIGATION_TIME = 1		# Temps de reg
 
 # Valors d'humitat
-DRY = 0
-WET = 1
+DRY = 0  # Sec
+WET = 1  # Humit
 
 # Paràmetres de connexió al servidor
 url = 'http://192.168.1.134:8080'
@@ -24,48 +24,54 @@ payload = {'date': time(), 'moisture': [0, 1]}
 
 # Situació dels pins per a cada component
 RELAY_PINS = []
-RELAY_PINS.append("BOARD11")
-RELAY_PINS.append("BOARD12")
+BUTTON_PINS = []
+SOIL_SENSOR_PINS = []
+SOIL_SENSOR_VCC_PINS = []
+
+RELAY_PINS.append("BOARD11") # Relé 0
+RELAY_PINS.append("BOARD12") # Relé 1
+
+BUTTON_PINS.append("BOARD16") # Botó 0
+BUTTON_PINS.append("BOARD18") # Botó 1
+
+SOIL_SENSOR_PINS.append("BOARD31") # Sensor d'humitat del sòl 0
+SOIL_SENSOR_PINS.append("BOARD35") # Sensor d'humitat del sòl 1
+
+SOIL_SENSOR_VCC_PINS.append("BOARD29") # Alimentació del sensor d'humitat del sòl 0
+SOIL_SENSOR_VCC_PINS.append("BOARD33") # Alimentació del sensor d'humitat del sòl 1
+
 RELAY_PINS = np.array(RELAY_PINS)
+BUTTON_PINS = np.array(BUTTON_PINS)
+SOIL_SENSOR_PINS = np.array(SOIL_SENSOR_PINS)
+SOIL_SENSOR_VCC_PINS = np.array(SOIL_SENSOR_VCC_PINS)
 
-BUTTON_0 = "BOARD16"
-BUTTON_1 = "BOARD18"
-
-SENSOR0 = "BOARD31"
-SENSOR1 = "BOARD35"
-SENSOR0_VCC = "BOARD29"
-SENSOR1_VCC = "BOARD33"
 
 # Creació dels OBJECTES.
 relays = []
+buttons = []
+soil_sensors = []
+soil_sensors_vcc = []
+
 for i in range(SYS_SIZE):
 	relays.append(gpiozero.OutputDevice(RELAY_PINS[i], active_high=False, initial_value=False))
+	buttons.append(gpiozero.Button(BUTTON_PINS[i]))
+	soil_sensors.append(gpiozero.DigitalInputDevice(SOIL_SENSOR_PINS[i], pull_up=None, active_state=False))
+	soil_sensors_vcc.append(gpiozero.OutputDevice(SOIL_SENSOR_VCC_PINS[i], active_high=True, initial_value=False))
+
 relays = np.array(relays)
-
-buttons = []
-buttons.append(gpiozero.Button(BUTTON_0))
-buttons.append(gpiozero.Button(BUTTON_1))
 buttons = np.array(buttons)
-
-sensors = []
-sensors.append(gpiozero.DigitalInputDevice(SENSOR0, pull_up=None, active_state=False))
-sensors.append(gpiozero.DigitalInputDevice(SENSOR1, pull_up=None, active_state=False))
-sensors = np.array(sensors)
-
-sensors_vcc = []
-sensors_vcc.append(gpiozero.OutputDevice(SENSOR0_VCC, active_high=True, initial_value=False))
-sensors_vcc.append(gpiozero.OutputDevice(SENSOR1_VCC, active_high=True, initial_value=False))
-sensors_vcc = np.array(sensors_vcc)
+soil_sensors = np.array(sensors)
+soil_sensors_vcc = np.array(sensors_vcc)
 
 
 # Variables GLOBALS
 valves_t0 = []
-valves_t0.append(0) # Darrer instant que la vàlvula 0 ha estat oberta.
-valves_t0.append(0) # Darrer instant que la vàlvula 1 ha estat oberta.
-valves_t0 = np.array(valves_t0, dtype='f')
-
 valves_t1 = []
-valves_t1.append(0) # Darrer instant que la vàlvula 0 ha estat tancada.
-valves_t1.append(0) # Darrer instant que la vàlvula 1 ha estat tancada.
+
+for i in range(SYS_SIZE):
+	valves_t0.append(0) # Darrer instant que les vàlvules han estat obertes
+	valves_t1.append(0) # Darrer instant que les vàlvules han estat tancades
+
+valves_t0 = np.array(valves_t0, dtype='f')
 valves_t1 = np.array(valves_t1, dtype='f')
 
