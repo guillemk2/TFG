f2318a8e620ce595e62f71f0f218443171026550
Guillem Peñafiel i Ràmia <pengu04@gmail.com>
2020-11-28 01:17:34 +0100

    Intgració II

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv/config');
+require("dotenv").config({ path: '/home/pi/Documents/.env' }); 
 const app = express();
 
 // DB connection
