a869f9973e121c9aaebcbcb0248488025c619ed5
Guillem Peñafiel i Ràmia <pengu04@gmail.com>
2020-11-30 13:24:35 +0100

    script3

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@
 git --work-tree=/home/pi/TFG/ --git-dir=/home/pi/TFG/.git pull origin dev
 
 # Posar en marxa servidor des d'ubicació remota
-npm --prefix ~/Documents/TFG/server/ run start
+npm --prefix ~/TFG/server/ run start
 
 # Posar en marxa prototip:
 python3 /home/pi/TFG/prototip/main.py
