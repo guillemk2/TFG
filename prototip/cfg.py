17f9dd4f049da2bdeeb4e8d9ee1175c505e09cf9
Guillem Peñafiel i Ràmia <pengu04@gmail.com>
2020-12-04 16:47:29 +0100

    prova humitat nou sòl

--- prototip/cfg.py
+++ prototip/cfg.py
@@ -11,7 +11,7 @@ import adafruit_dht
 SYS_SIZE = 2
 
 # Duració dels events (en segons)
-POLL_FREQUENCY = 60*10	# Freqüència de lectura dels sensors (30 minuts)
+POLL_FREQUENCY = 60*5	# Freqüència de lectura dels sensors (30 minuts)
 POLL_TIME = 0.75		# Duració de la lectura dels sensors
 IRRIGATION_TIME = 10	# Temps de reg
 BOUNCE_TIME = 0.25		# Temps de rebot dels botons (mínim temps de reg)
