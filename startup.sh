b89ec3d3e03c1f40590cecf544376bd7874a10ab
Guillem Peñafiel i Ràmia <pengu04@gmail.com>
2020-12-06 17:51:30 +0100

    prometheus instal·lació

--- startup.sh
+++ startup.sh
@@ -6,5 +6,12 @@ git --work-tree=/home/pi/TFG/ --git-dir=/home/pi/TFG/.git pull origin dev
 # Posar en marxa servidor des d'ubicació remota
 npm --prefix ~/TFG/server/ run start &
 
+# Posar en marxa prometheus
+/usr/local/bin/prometheus \
+    --config.file /etc/prometheus/prometheus.yml \
+    --storage.tsdb.path /var/lib/prometheus/ \
+    --web.console.templates=/etc/prometheus/consoles \
+    --web.console.libraries=/etc/prometheus/console_libraries
+
 # Posar en marxa prototip:
 python3 /home/pi/TFG/prototip/main.py
