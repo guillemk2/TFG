45d008ba8b16a02da1edc06821cbfdf7c174cd14
Guillem Peñafiel i Ràmia <pengu04@gmail.com>
2020-12-06 15:39:04 +0100

    Prometheus: variables per cada test 1

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,10 +2,21 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/post');
 
+// Mida del sistema (nombre de testos)
+const SYS_SIZE = 2
+
 // Prometheus metrics
 const client = require('prom-client');
 const collectDefaultMetrics = client.collectDefaultMetrics;
 const temp = new client.Gauge({ name: 'temp', help: 'Temperatura ambient' });
+
+var moisture = []
+var irrigation = []
+
+for (i = 0; i < SYS_SIZE; i++) {
+	moisture.push(new client.Gauge({ name: 'moisture_' + i, help: 'Humitat en test ' + i }));
+	irrigation.push(new client.Gauge({ name: 'irrigation_' + i, help: 'Reg en test ' + i }));
+}
 
 router.get('/', (req, res) => {
 	
@@ -23,7 +34,11 @@ router.post('/', async (req, res) => {
 	
 	try {
 		// Prometheus
-		temp.set(req.body.temperature); // Set to 10
+		temp.set(req.body.temperature);
+		for (i = 0; i < SYS_SIZE; i++) {
+			moisture[i].set(req.body.plants[i].moisture);
+			irrigation[i].set(req.body.plants[i].irrigation);
+		}
 
 		// MongoDB
 		const post = new Post({
